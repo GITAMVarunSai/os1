6c9d0ae7d3cff66fa3023be1c944933380cb7179
GITAMVarunSai <vandra@gitam.in>
2025-03-25 15:06:25 +0530

    Create FCFS.c

---
+++ FCFS.c
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int size = 8;
+void FCFS(int arr[], int head)
+{
+	int seek_count = 0;
+	int distance, cur_track;
+
+	for (int i = 0; i < size; i++) {
+		cur_track = arr[i];
+		distance = abs(cur_track - head);
+		seek_count += distance;
+		head = cur_track;
+	}
+
+	cout << "Total number of seek operations = "<< seek_count << endl;
+	cout << "Seek Sequence is" << endl;
+	for (int i = 0; i < size; i++) {
+		cout << arr[i] << endl;
+	}
+}
+int main()
+{
+
+	int arr[size] = {  98, 183, 37, 122, 14, 124, 65, 67 };
+	int head = 53;
+
+	FCFS(arr, head);
+
+	return 0;
+}
