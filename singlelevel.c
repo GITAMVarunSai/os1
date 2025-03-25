0d06650d24f481491c153e6f1b42ab812a571df8
GITAMVarunSai <vandra@gitam.in>
2025-03-25 14:30:50 +0530

    Create singlelevel.c

---
+++ singlelevel.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <string.h>
+
+void main() {
+    int nf = 0, i = 0, j = 0, ch;
+    char mdname[10], fname[10][10], name[10];
+    
+    printf("Enter the directory name: ");
+    scanf("%s", mdname);
+
+    printf("Enter the number of files: ");
+    scanf("%d", &nf);
+
+    do {
+        printf("Enter file name to be created: ");
+        scanf("%s", name);
+
+        for (i = 0; i < nf; i++) {
+            if (!strcmp(name, fname[i])) {
+                break; // If the file already exists, break the loop.
+            }
+        }
+
+        if (i == nf) {
+            strcpy(fname[j++], name); // Add new file name
+            nf++;
+        } else {
+            printf("There is already %s\n", name);
+        }
+
+        printf("Do you want to enter another file (yes: 1 or no: 0): ");
+        scanf("%d", &ch);
+
+    } while (ch == 1);
+
+    printf("Directory name is: %s\n", mdname);
+    printf("Files names are: ");
+    for (i = 0; i < j; i++) {
+        printf("\t %s", fname[i]);
+    }
+    getch();  // Pause the console.
+}
